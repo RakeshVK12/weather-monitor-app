a42349f1702659ebe85a0001a6661939111a89a2
RakeshVK12 <rakeshtpr2000@gmail.com>
2025-05-03 21:08:03 +0530

    check

--- src/main/java/com/example/weather_monitor/WeatherMonitorApplication.java
+++ src/main/java/com/example/weather_monitor/WeatherMonitorApplication.java
@@ -3,7 +3,7 @@ package com.example.weather_monitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+//Rakesh
 
 @SpringBootApplication
 @EnableScheduling
